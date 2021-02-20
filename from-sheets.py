4dac494c2f2126cc780acef9bee852eabd123279
Gaurav Kale <gauravkale100@gmail.com>
2021-02-20 06:30:57 -0800

    Initial from Google sheets to PD Dataframe
    Use survey_to_df() function to do this

---
+++ from-sheets.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+import pickle
+import os.path
+import pandas as pd
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1mD8i0fRzlQW-giZPPkC0Ofm_E9EvF3nXZlZTxH_XX40'
+DATA_TO_PULL = 'FORM RESPONSES 1'
+
+def gsheet_api_check(SCOPES):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+def pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL):
+    creds = gsheet_api_check(SCOPES)
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range=DATA_TO_PULL).execute()
+    values = result.get('values', [])
+    
+    if not values:
+        print('No data found.')
+    else:
+        rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                  range=DATA_TO_PULL).execute()
+        data = rows.get('values')
+        print("COMPLETE: Data copied")
+        return data
+
+
+
+def survey_to_df():
+    data = pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL)
+    df = pd.DataFrame(data[1:], columns=data[0])
+    
+    
+
+if __name__ == '__main__':
+    main()
