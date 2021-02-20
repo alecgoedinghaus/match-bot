57ee8539a8eff08ecf19cb454994a876722a72cd
jlimcode <56215940+jlimcode@users.noreply.github.com>
2021-02-20 03:41:39 -0800

    Development Environment Setup

---
+++ example_bot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(BOT_TOKEN)
