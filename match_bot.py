be82741823c9d95a4d71095ca9caa32ed0b47771
jlimcode <56215940+jlimcode@users.noreply.github.com>
2021-02-20 18:58:17 -0800

    User Discovery Fix
    Non-working lookup table implementation. Working member discovery.

--- match_bot.py
+++ match_bot.py
@@ -7,7 +7,10 @@ import matcher
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -15,21 +18,33 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
 
+
 @bot.command()
 async def survey(ctx):
     users = ctx.message.mentions
     for user in users:
         await user.send('https://forms.gle/n7xtMYnZEqwPQ1p67')
 
+
 @bot.command()
 async def match(ctx):
     print("running matching...")
     pref_data = from_sheets.survey_to_df()
-    print(pref_data)
-    #print(matcher.convert_categorical(pref_data))
+    # print(pref_data)
     matrix = matcher.convert_categorical(pref_data)
     pairs = matcher.pair(matrix)
-    print(pairs)
-    print(list(matcher.solve_stm(pairs)))
+    # print(pairs)
+    pair_generator = matcher.solve_stm(pairs)
+    print(list(pair_generator))
+    lookup_table = make_reverse_lookup(ctx.guild.members)
+    print(lookup_table)
+    # for adam, eve in pair_generator:
+    #     print(lookup_table[adam])
+    #     print(lookup_table[eve])
+
+
+def make_reverse_lookup(member_list):
+    return {member.name : member.id for member in member_list}
+
 
 bot.run(BOT_TOKEN)
