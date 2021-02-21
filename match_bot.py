beec433793e1b1fb002aca42fda471d9095c6869
jlimcode <56215940+jlimcode@users.noreply.github.com>
2021-02-20 20:14:05 -0800

    Finish pairing, channel creation, invites

--- match_bot.py
+++ match_bot.py
@@ -38,13 +38,47 @@ async def match(ctx):
     print(list(pair_generator))
     lookup_table = make_reverse_lookup(ctx.guild.members)
     print(lookup_table)
-    # for adam, eve in pair_generator:
-    #     print(lookup_table[adam])
-    #     print(lookup_table[eve])
+    for pair in clean_list(pair_generator):
+        channel_name = generate_channel_name(pair)
+        chat_channel = await ctx.guild.create_text_channel(channel_name)
+        for member_player in pair:
+            member_name = member_player.name
+            print(member_name)
+            member = lookup_table.get(member_name)
+            invite = await chat_channel.create_invite()
+            await member.send("invite link: " + invite.url)
 
 
 def make_reverse_lookup(member_list):
-    return {member.name : member.id for member in member_list}
+    return {member.name + "#" + member.discriminator: member for member in member_list}
+
+
+def clean_list(pair_generator):
+    sent_list = []
+    cleaned_list = []
+    for pair in pair_generator:
+        if is_valid_pair(pair, sent_list):
+            cleaned_list.append(pair)
+            for member in pair:
+                sent_list.append(member.name)
+    return cleaned_list
+
+
+def generate_channel_name(pair_of_players):
+    out = ""
+    out += pair_of_players[0].name.split("#")[0]
+    out += "--"
+    out += pair_of_players[1].name.split("#")[0]
+    return out
+
+
+def is_valid_pair(pair_of_players, sent_list):
+    for player in pair_of_players:
+        if player is None:
+            return False
+        elif player.name in sent_list:
+            return False
+    return True
 
 
 bot.run(BOT_TOKEN)
